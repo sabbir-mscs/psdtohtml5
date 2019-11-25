655cdf511c4d1f07372828d9c92d10f8bc97e805
Sabbir Ahmed Saif <sabbirahmedsaif@gmail.com>
2019-11-25 19:08:37 +0600

    Changed Index, Style, JQuery animation.

--- js/main.js
+++ js/main.js
@@ -1,41 +1,51 @@
-$(document).ready (function() {
-    
-    new WOW().init();
-    
-    
-    $(".burger-nav").on("click", function(){
-        
-        $(".container nav ul").toggleClass("open");
-        
-    });
-    
-    
-    
-    
-   $('.js-wp-2').waypoint(function(direction) {
-       
-       $('.js-wp-2').addClass('animated slideInUp');
-       
-   }, {
-		offset: '70%'
-	});
-    
-    $('.js-wp-3').waypoint(function(direction) {
-       
-       $('.js-wp-3').addClass('animated fadeIn');
-       
-   }, {
-		offset: '70%'
-	});
-    
-   
-    
-    
-    
-    $(".iphone-btn").delay(2300).animate( {bottom: "+=-3"}, 300);
-    $(".iphone-btn").delay(300).animate( {top: "+=-3"}, 100);
-    
-    
-    
-    
+$(document).ready(function() {
+  //! Initialize WOW
+  new WOW().init();
+
+  //?iphone appears
+  $(".js-wp-2").waypoint(
+    function(direction) {
+      $(".js-wp-2").addClass("animated bounceInUp");
+    },
+    {
+      offset: "100%"
+    }
+  );
+  //? Button press
+  $(".iphone-btn")
+    .delay(2300)
+    .animate(
+      {
+        bottom: "+=-3"
+      },
+      300
+    );
+
+  $(".iphone-btn")
+    .delay(300)
+    .animate(
+      {
+        top: "+=-3"
+      },
+      300
+    );
+  //?iPhone Display On
+  $(".js-wp-3").waypoint(
+    function(direction) {
+      $(".js-wp-3").addClass("animated fadeIn");
+    },
+    {
+      offset: "100%"
+    }
+  );
+
+  //!Section Appearance
+  $(".section-appear").waypoint(
+    function(direction) {
+      $(".section-appear").addClass("animated fadeIn");
+    },
+    {
+      offset: "100%"
+    }
+  );
 });
